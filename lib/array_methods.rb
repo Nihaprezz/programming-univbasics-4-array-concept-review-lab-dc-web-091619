12e936b81cfb16c5154139061e9218e166720189
Sergio Perez <sergio13prez@gmail.com>
2019-08-12 00:30:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,4 @@
-def find_element_index(array, value_to_find)
+=begin def find_element_index(array, value_to_find)
   # Add your solution here
   counter = 0 
 
@@ -8,6 +8,11 @@ def find_element_index(array, value_to_find)
     end
     counter += 1
   end
+=end end
+
+def find_element_index(array, value_to_find)
+  # Add your solution here
+  array.find_index(value_to_find)
 end
 
 def find_max_value(array)
